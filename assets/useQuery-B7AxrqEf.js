024df7b67ca151010e03a71cef636d47890803ed
Alex Lema Marques <alejandrolemamarques@gmail.com>
2025-04-11 23:16:57 +0200

    Updates

---
+++ assets/useQuery-B7AxrqEf.js
@@ -0,0 +1,1 @@
+import{u as r,Q as u}from"./useBaseQuery-B8Kr4lM1.js";function a(e,s){return r(e,u)}export{a as u};
--- assets/useQuery-BQEVlMt-.js
+++
@@ -1,1 +0,0 @@
-import{u as r,Q as u}from"./useBaseQuery-Bqz_ihlr.js";function a(e,s){return r(e,u)}export{a as u};
